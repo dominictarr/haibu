2ad1cc5a9fcc53a80570c9cace3e4e5ea8a50811
Dominic Tarr <dominic.tarr@gmail.com>
2011-10-10 17:40:13 +1100

    add support for stopping an app instance.

--- test/streaming/deploy-test.js
+++ test/streaming/deploy-test.js
@@ -42,7 +42,7 @@ vows.describe('haibu/deploy')
     topic: function () {
       var that = this
       var deployStream = fs.createReadStream(helloTarball)
-      var reqStream = request({url: 'http://localhost:9011/deploy/test/hellonode', method:'PUT'}
+      var reqStream = request({url: 'http://localhost:9011/deploy/test/hellonode/' + Date.now(), method:'PUT'}
         , function (err, res, body) {
         try { appPort = JSON.parse(body).port } catch (err) {console.error(err)} //just assign this stuff, it's tested next.
         that.callback(err, res, body)
@@ -79,7 +79,7 @@ vows.describe('haibu/deploy')
     topic: function () {
       var that = this
       var deployStream = fs.createReadStream(brokenTarball)
-      var reqStream = request({url: 'http://localhost:9011/deploy/test/hellonode', method:'PUT'}
+      var reqStream = request({url: 'http://localhost:9011/deploy/test/hellonode/' + Date.now(), method:'PUT'}
         , function (err, res, body) {
         try { appPort = JSON.parse(body).port } catch (err) {console.error(err)} //just assign this stuff, it's tested next.
         that.callback(err, res, body)
